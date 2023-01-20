81aab100d909c4d7ea66d167a8817ca9f56bec22
Matthew Powell <matt.powell2784@gmail.com>
2023-01-20 20:14:26 +0000

    cors logic added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const connectDb = require('./connectDb')
 const highScoresRoute = require('./routes/highScores')
 
